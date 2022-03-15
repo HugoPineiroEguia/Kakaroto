5a1c105c5a91a707256d6e244a20a4411ac38f5b
pinha <pinhaeguia@gmail.com>
2022-03-15 10:12:45 +0100

    NOOOOOOOOOOOOOOOOOOOOO

--- src/Mainclass.java
+++ src/Mainclass.java
@@ -2,5 +2,6 @@ public class Mainclass {
     public static void main(String[] args) {
         System.out.println("Broly anda OP");
         System.out.println("Gohan se nos unio a la PARY");
+        System.out.println("NOOOOOOOOOOOOOOOOOOOOOOOOO");
     }
 }
