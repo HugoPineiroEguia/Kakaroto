91e310f941fb9d0cfa8c1be6cbd9d896c238b6ce
pinha <pinhaeguia@gmail.com>
2022-03-15 10:11:58 +0100

    Gohan se nos fue

--- src/Mainclass.java
+++ src/Mainclass.java
@@ -2,5 +2,6 @@ public class Mainclass {
     public static void main(String[] args) {
         System.out.println("Broly anda OP");
         System.out.println("Gohan");
+        System.out.println("Ya valio madres");
     }
 }
